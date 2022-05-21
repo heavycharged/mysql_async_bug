7cd5d20f7ae9a2183336cbe8fa8a57a93231afc3
heavycharged <heavycharged@gmail.com>
2022-05-21 12:39:11 +0300

    more than 'no noise'

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,35 @@
-use std::time::Duration;
-use actix_web::{App, Responder, web};
-use mysql_async::{Pool};
-use mysql_async_bug::AppState;
-use actix_web::{HttpServer, get};
-use log::{error, debug, warn};
+use actix_web::{web::Data, App, get, HttpServer};
+use mysql_async::{Pool, prelude::*};
 
+#[actix_web::main]
+async fn main() {
+    let mysql_addr = std::env::var("MYSQL_ADDR").unwrap_or_else(|_| "mysql:3306".into());
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    pretty_env_logger::init();
+    let dsn = format!("mysql://{}:{}@{}/{}", "root", "root", mysql_addr, "test");
 
-    // spawn some mysql instance in docker, e.g.:
-    // docker run -it -d --name mysql_test -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test -p 127.0.0.1:3306:3306 mysql
-    // dont forget to provide MYSQL_ADDR env variable with host:port pairs.
+    let pool = Pool::from_url(&dsn).unwrap();
 
-    debug!("starting application...");
-
-    let mysql_addr = std::env::var("MYSQL_ADDR").unwrap_or_else(|_| "mysql:3306".to_string());
-
-    let dsn = format!(
-        "mysql://{}:{}@{}/{}",
-        "root", "root", mysql_addr, "test"
-    );
-
-    let pool = Pool::from_url(&dsn)?;
-
-    let app = AppState::new(pool);
-
-    let data = web::Data::new(app);
-    let data_clone = data.clone();
-
-    debug!("starting http server...");
-    let result = HttpServer::new(move || {
-        App::new()
-            .app_data(data.clone())
-            .service(load_from_db)
-    }).bind(("0.0.0.0", 8080))?
+    let pool2 = pool.clone();
+    HttpServer::new(move || App::new().app_data(Data::new(pool.clone())).service(index))
+        .bind(("0.0.0.0", 8080))
+        .unwrap()
         .run()
-        .await;
+        .await
+        .unwrap();
 
-    if let Err(e) = result {
-        error!("http server stopped with error: {}", e);
-    }
+    actix_web::rt::time::sleep(std::time::Duration::from_secs(3)).await;
 
-    debug!("sleep for 3 secs...");
-    tokio::time::sleep(Duration::from_secs(3)).await;
-    debug!("sleep done");
-
-    if let Err(e) = data_clone.into_inner().shutdown().await {
-        error!("cannot shutdown mysql: {}", e);
-    }
-    debug!("WOOHOO! pool was shutdown finally (or you forgot to make at least one request)");
-
-    Ok(())
+    pool2.disconnect().await.unwrap()
 }
 
 #[get("/")]
-async fn load_from_db(data: web::Data<AppState>) -> impl Responder {
-    let number = data.select_number().await;
+async fn index(pool: Data<Pool>) -> String {
+    let conn = pool.get_conn().await.unwrap();
 
-    match number {
-        Ok(v) => v.to_string(),
-        Err(e) => {
-            warn!("error occurred: {}", e);
-            e.to_string()
-        }
-    }
+    "SELECT 42"
+        .first::<u32, _>(conn)
+        .await
+        .unwrap()
+        .map(|i| i.to_string())
+        .unwrap_or_else(|| "cannot query static number".into())
 }
